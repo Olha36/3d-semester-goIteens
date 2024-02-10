8b0fe61978d5fec0dec89a3d11d3034748f96d38
Olha36 <olhakucheruk36@gmail.com>
2024-02-10 13:36:53 +0200

    switcher works

--- script.js
+++ script.js
@@ -10,18 +10,13 @@ document.querySelector('.more-button-list-item img').addEventListener('click', f
   document.querySelector('.interactive-modal').classList.toggle('interactive-active');
 })
 
-document.querySelector('.switcher-circle-day').addEventListener('click', function () {
-  console.log('hi day button');
+document.querySelector('.switcher').addEventListener('click', function () {
+  console.log('hi switcher');
   document.querySelector('.switcher-circle-night').classList.toggle('switcher-active'); 
-  document.querySelector('.switcher-circle-day').classList.toggle('switcher-non-active');
+  document.querySelector('.switcher-circle-day').classList.toggle('switcher-non-active'); 
 })
 
-document.querySelector('.switcher-circle-night').addEventListener('click', function () {
-  console.log('hi night button');
-  document.querySelector('.switcher-circle-day').classList.toggle('switcher-active'); 
-  document.querySelector('.switcher-circle-day').style.opacity='1'
-  document.querySelector('.switcher-circle-night').classList.toggle('switcher-non-active');
-})
+
 
 
 
