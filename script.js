e1e715854a743c55826d81885c79153db5b5afff
Olha36 <olhakucheruk36@gmail.com>
2024-02-10 17:12:32 +0200

    modal window

--- script.js
+++ script.js
@@ -14,8 +14,12 @@ document.querySelector('.switcher').addEventListener('click', function () {
   console.log('hi switcher');
   document.querySelector('.switcher-circle-night').classList.toggle('switcher-active'); 
   document.querySelector('.switcher-circle-day').classList.toggle('switcher-non-active'); 
+  document.querySelector('.switcher-modal').classList.toggle('modal-active'); 
 })
 
+document.querySelector('.cross-menu').addEventListener('click', function() {
+  document.querySelector('.switcher-modal').classList.remove('modal-active'); 
+})
 
 
 
