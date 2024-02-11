727e00980fe1296307efb6b09d2ca94ea55b9d30
Olha36 <olhakucheruk36@gmail.com>
2024-02-11 11:17:59 +0200

    add function for a leap year

---
+++ js/birth-year.js
@@ -0,0 +1,21 @@
+// const year = document.querySelector('.birth-year').value;
+const year = document.querySelector('.input-wrapper input');
+const inputValue = document.querySelector('.input-name').value;
+// year.textContent =  
+  console.log(year);
+  console.log(inputValue);
+
+// document.querySelector('.search').addEventListener('click', function() {
+  
+
+//   function isLeapYear(year) {
+//     return (year % 4 === 0 && year % 100 !==0) ||(year % 400 === 0);
+    
+//   }
+  
+//   if (isLeapYear(year)) {
+//     console.log(`Year ${year} is a leap year`);
+//   } else {
+//     console.log(`Year ${year} is not a leap year`);
+//   }
+// })
