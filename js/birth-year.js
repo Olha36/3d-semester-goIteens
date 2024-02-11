52de1a181079348d0ffe049e5bafe0fbd7fdc76c
Olha36 <olhakucheruk36@gmail.com>
2024-02-11 12:00:00 +0200

    finish birth-year

--- js/birth-year.js
+++ js/birth-year.js
@@ -1,21 +1,18 @@
-// const year = document.querySelector('.birth-year').value;
-const year = document.querySelector('.input-wrapper input');
-const inputValue = document.querySelector('.input-name').value;
-// year.textContent =  
-  console.log(year);
-  console.log(inputValue);
+let year = document.querySelector('.year-heading').textContent;
+document.querySelector('.search').addEventListener('click', function() {
+    year =  document.querySelector('.input-wrapper input').value;
+    year.value = "";
 
-// document.querySelector('.search').addEventListener('click', function() {
-  
-
-//   function isLeapYear(year) {
-//     return (year % 4 === 0 && year % 100 !==0) ||(year % 400 === 0);
+  function isLeapYear(year) {
+    return (year % 4 === 0 && year % 100 !==0) ||(year % 400 === 0);
     
-//   }
+  }
   
-//   if (isLeapYear(year)) {
-//     console.log(`Year ${year} is a leap year`);
-//   } else {
-//     console.log(`Year ${year} is not a leap year`);
-//   }
-// })
+  if (isLeapYear(year)) {
+    document.querySelector('.year-heading').textContent = `Ви народилися у високосний ${year} рік!`
+    document.querySelector('.year-heading').style.color = '#039900';
+  } else {
+    document.querySelector('.year-heading').textContent = `Ви народилися не у високосний ${year} рік!`
+    document.querySelector('.year-heading').style.color = 'red';
+  }
+})
