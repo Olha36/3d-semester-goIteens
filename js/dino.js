272dbdd6008921ad5b98f74cd987d0bf54bcf8d9
Olha36 <olhakucheruk36@gmail.com>
2024-02-14 12:14:41 +0200

    create dino and cactus + animation

---
+++ js/dino.js
@@ -0,0 +1,15 @@
+const dino = document.querySelector('.dino');
+console.log(dino);
+
+function jump() {
+  if(dino.classList != "jump") {
+    dino.classList.add('jump');
+  }
+  setTimeout(function () {
+    dino.classList.remove('jump');
+  }, 300)
+}
+
+document.addEventListener("keydown", function(event) {
+  jump();
+})
