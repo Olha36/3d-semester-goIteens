7e71c720a9b79ea8359e0557b9d8cde8a1264610
Olha36 <olhakucheruk36@gmail.com>
2024-02-26 18:15:54 +0200

    football bug fixation

--- js/football.js
+++ js/football.js
@@ -1,34 +1,17 @@
 const footballWrapper = document.querySelector('.football-wrapper');
 const ball = document.querySelector('.ball');
 
-function moveBall(x, y) {
-  const footballRect = footballWrapper.getBoundingClientRect();
-  const ballRect = ball.getBoundingClientRect();
 
-  // Calculate the maximum allowable position for the ball within the container bounds
-  const maxX = footballRect.width - ballRect.width;
-  const maxY = footballRect.height - ballRect.height;
+footballWrapper.addEventListener('click', (event) => {
+  let newPositionLeft = event.clientX - footballWrapper.getBoundingClientRect().left;
+  let newPositionTop = event.clientY - footballWrapper.getBoundingClientRect().top;
+  console.log(newPositionTop, newPositionLeft);
 
-  // Calculate the exact position where the ball should move within the container
-  const targetX = x - footballRect.left - ballRect.width * 2;
-  const targetY = y - footballRect.top - ballRect.height * 2;
-
-  console.log('Target Position:', targetX, targetY); // Log target position
-
-  // Ensure the new position is within the bounds of the container
-  const boundedX = Math.min(Math.max(targetX, 0), maxX);
-  const boundedY = Math.min(Math.max(targetY, 0), maxY);
-
-  console.log('Bounded Position:', boundedX, boundedY); // Log bounded position
-
+  ball.style.top = newPositionTop - 25 + 'px'
+  ball.style.left = newPositionLeft - 25 + 'px';
   ball.style.transition = 'transform 0.5s ease';
-  ball.style.transform = `translate(${boundedX}px, ${boundedY}px)`;
-}
+})
 
-function logKey(e) {
-  moveBall(e.clientX, e.clientY);
-}
 
-footballWrapper.addEventListener('click', logKey);
 
 
