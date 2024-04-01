795b0ed23a832d9624943b3aa4ab19908acb6b4f
Olha36 <olhakucheruk36@gmail.com>
2024-04-01 14:01:17 +0300

    refactoring highest number file

--- js/highest-number.js
+++ js/highest-number.js
@@ -2,15 +2,18 @@ const numberOne = document.querySelector('.enter-number-one');
 const numberTwo = document.querySelector('.enter-number-two');
 const numberThree = document.querySelector('.enter-number-three');
 const highestNumber = document.querySelector('.find-biggest');
+const showEnteredNumber = document.querySelector('.your-number');
 
-highestNumber.addEventListener('click', function() {
+function showHighestNumber() {
   const getFirstValue = Number.parseInt(numberOne.value);
   const getSecondValue = Number.parseInt(numberTwo.value);
   const getThirdValue = Number.parseInt(numberThree.value);
   const highest = Math.max(getFirstValue, getSecondValue, getThirdValue);
 
-  document.querySelector('.your-number').textContent = `(${highest})`;
+  showEnteredNumber.textContent = `(${highest})`;
   numberOne.value = '';
   numberTwo.value = '';
   numberThree.value = '';
-})
+}
+
+highestNumber.addEventListener('click', showHighestNumber);
