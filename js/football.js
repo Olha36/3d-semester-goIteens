c83d0fd05798882f23f4d9aae947a5a64967be59
Olha36 <olhakucheruk36@gmail.com>
2024-05-18 17:38:47 +0300

    add rolling effect to the ball

--- js/football.js
+++ js/football.js
@@ -4,27 +4,32 @@ const ball = document.querySelector('.ball');
 function playFootball(event) {
   let newPositionLeft = event.clientX - footballWrapper.getBoundingClientRect().left;
   let newPositionTop = event.clientY - footballWrapper.getBoundingClientRect().top;
+  let currentLeft = parseInt(ball.style.left || 0, 10);
+  let currentTop = parseInt(ball.style.top || 0, 10);
+  let distance = Math.sqrt(Math.pow(newPositionLeft - currentLeft, 2) + Math.pow(newPositionTop - currentTop, 2));
+  let duration = distance / 200;
 
-  ball.style.top = newPositionTop - 25 + 'px'
-  ball.style.left = newPositionLeft - 25 + 'px';
-  ball.style.transition = 'transform 0.5s ease';
+  ball.style.transition = `transform ${duration}s linear, left ${duration}s linear, top ${duration}s linear`;
+  ball.style.left = newPositionLeft - ball.offsetWidth / 2 + 'px';
+  ball.style.top = newPositionTop - ball.offsetHeight / 2 + 'px';
+
+  let rotationAngle = (distance / ball.offsetWidth) * 360;
+  ball.style.transform = `rotate(${rotationAngle}deg)`;
 }
 
 function changeColor() {
-  let colors = ['#159ec6','#0c728f', '#45c4e9']; //
+  let colors = ['#159ec6', '#0c728f', '#45c4e9'];
   let index = 0;
 
-  colors.forEach((color, i) => {
-      setInterval(() => {
-      footballWrapper.style.backgroundColor = color;
-      index = (index + 1) % colors.length;
-      
-    }, 3000 * (i + 1));
-  });
-
+  setInterval(() => {
+    footballWrapper.style.backgroundColor = colors[index];
+    index = (index + 1) % colors.length;
+  }, 3000);
 }
+
 changeColor();
 footballWrapper.addEventListener('click', playFootball);
+
 
 
 
